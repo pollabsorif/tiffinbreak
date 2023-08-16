38fde3c0acb0027861db6a969f1865c65983a1e6
pollabsorif <142155625+pollabsorif@users.noreply.github.com>
2023-08-16 19:56:13 +0530

    Delete routes directory

--- routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/orderRoutes.js
-const express = require('express');
-const router = express.Router();
-const orderController = require('../controllers/orderController');
-
-router.post('/orders', orderController.create);
-router.get('/orders/:userId', orderController.getAllByUser);
-
-module.exports = router;
